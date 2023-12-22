d39e32127404602c640687af07a02c0b9b493ae7
Jeongbin828 <103910367+Jeongbin828@users.noreply.github.com>
2023-12-22 13:52:57 +0900

    Initial commit

--- src/main/java/inhatc/spring/shop/controller/HelloController.java
+++ src/main/java/inhatc/spring/shop/controller/HelloController.java
@@ -1,5 +1,6 @@
 package inhatc.spring.shop.controller;
 
+import inhatc.spring.shop.dto.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,11 @@ import java.awt.*;
 public class HelloController {
 //    @RequestMapping(value = "/", method = RequestMethod.GET)
     @GetMapping("/")
-    public String hello(){
-        return "hello";
+    public UserDto hello(){
+        UserDto userDto = UserDto.builder().name("김길동").age(10).build();
+        userDto.setAge(20);
+        userDto.setName("홍길동");
+        System.out.println("UserDto : " + userDto);
+        return userDto;
     }
 }
---
+++ src/main/java/inhatc/spring/shop/dto/UserDto.java
@@ -0,0 +1,14 @@
+package inhatc.spring.shop.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+    private int age;
+    private String name;
+}
