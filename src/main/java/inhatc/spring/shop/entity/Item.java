3f059c93e0c97eea7d2c1e3a5bd8215e1e1ff558
Jeongbin828 <103910367+Jeongbin828@users.noreply.github.com>
2023-12-26 23:23:51 +0900

    Initial commit

--- src/main/java/inhatc/spring/shop/entity/Item.java
+++ src/main/java/inhatc/spring/shop/entity/Item.java
@@ -1,8 +1,7 @@
 package inhatc.spring.shop.entity;
 
 import inhatc.spring.shop.constant.ItemSellStatus;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -16,14 +15,18 @@ import java.time.LocalDateTime;
 @Builder
 public class Item {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
     private Long id;                        // 상품 아이디
 
+    @Column(nullable = false, length = 50)
     private String itemNm;                  // 상품 이름
 
     private int price;                      // 가격
 
     private int stockNumber;                // 재고 수량
 
+    @Column(nullable = false)
     private String itemDetail;              // 상품 상세 설명
     
     private ItemSellStatus itemSellStatus;  // 상품 판매 상태
