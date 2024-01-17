36607e93edc427dbb6d253fa2abc32cddb91b0a9
Jeongbin828 <103910367+Jeongbin828@users.noreply.github.com>
2024-01-17 15:48:24 +0900

    Initial commit

--- src/main/java/inhatc/spring/shop/controller/ThymeleafController.java
+++ src/main/java/inhatc/spring/shop/controller/ThymeleafController.java
@@ -1,13 +1,17 @@
 package inhatc.spring.shop.controller;
 
 import inhatc.spring.shop.dto.ItemDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.awt.*;
 
 @Controller
+@Slf4j
+//@RequestMapping("/thymeleaf")
 public class ThymeleafController {
     @GetMapping("/thymeleaf/ex1")
     public String ex1(Model model, ItemDto dto){
@@ -25,5 +29,11 @@ public class ThymeleafController {
     @GetMapping("/thymeleaf/ex2")
     public String ex2(){
         return "thymeleaf/ex2";
+    }
+
+    @GetMapping("/thymeleaf/ex3")
+    public String ex3(String name, int age){
+        log.info("name : " + name + ", age : " + age);
+        return "thymeleaf/ex3";
     }
 }
